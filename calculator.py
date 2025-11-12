233e8dd27a221778d9aade6391d6d9bb9afd4598
mfarmer5 <mahakfarmer1@gmail.com>
2025-11-12 17:11:07 -0500

    first commit

--- calculator.py
+++ calculator.py
@@ -5,7 +5,27 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
 def add(a, b): 
-    pass
+    return a + b
+def sub(a,b ):
+    return a - b
+def mul(a,b):
+    return a * b
+def div(a,b):
+    if a == 0:
+        raise ValueError("Can't divide by zero.")
+    return b/a
+def log(a,b):
+    if a <= 0 or a == 1:
+        raise ValueError("Base must be greater than zero and not equal to one")
+    if b <= 0:
+        raise ValueError("b must be greater than zero.")
+    return  math.log(b, a)
+def exp(a, b):
+    return a ** b
+
+
 
 
