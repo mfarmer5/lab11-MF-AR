13459784fdb8aadef981e3cf39c5637ae197b9e3
mfarmer5 <mahakfarmer1@gmail.com>
2025-11-18 09:43:23 -0500

    commit p1

--- calculator.py
+++ calculator.py
@@ -13,7 +13,7 @@ def hypotenuse(a, b):
     return math.hypot(a, b)
 def add(a, b): 
     return a + b
-def sub(a,b ):
+def subtract(a,b ):
     return a - b
 def mul(a,b):
     return a * b
@@ -21,7 +21,7 @@ def div(a,b):
     if a == 0:
         raise ValueError("Can't divide by zero.")
     return b/a
-def log(a,b):
+def logarithm(a,b):
     if a <= 0 or a == 1:
         raise ValueError("Base must be greater than zero and not equal to one")
     if b <= 0:
